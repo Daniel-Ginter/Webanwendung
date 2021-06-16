5823204e7f0b73c8baa4d4adc7caa53e35f7b748
Speick <44347258+Speick98@users.noreply.github.com>
2021-06-16 13:41:31 +0200

    routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const glob = require("glob");
+
+
+var getDirectories = function (src, callback) {
+ glob(src + '/*/**/db.json', callback);
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/db', function(req, res, next) {
+  getDirectories('public/sites',function (err, resu) {
+    if (err) {
+      console.log('Error', err);
+    } else {
+      res.send(resu);
+    }
+  });
 });
 
 module.exports = router;
